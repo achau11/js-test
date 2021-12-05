be0a80231502bff5fb75acd23c8a5d4e369da0a7
xazzer81 <achau1019@gmail.com>
2021-12-04 19:23:21 -0500

    Make request to backend, fix prettier/eslint bugs, make a temporary list component

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,25 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import './App.css';
+import Repo from './components/Repo';
 
 export function App() {
+  const [repos, setRepos] = useState([]);
+  //API call
+  useEffect(() => {
+    axios.get('http://localhost:4000/repos').then((res) => setRepos(res.data));
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>hello</h1>
+      <Repo />
+      {console.log(repos)}
     </div>
   );
 }
---
+++ web/src/components/Repo.jsx
@@ -0,0 +1,12 @@
+function Repo() {
+  return (
+    <div className="repo">
+      <span>Name</span>
+      <span>Location</span>
+      <span>Type</span>
+      <span>Value</span>
+    </div>
+  );
+}
+
+export default Repo;
