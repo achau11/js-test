7e8bf4193e301b395da0b3d359e0a3074a409996
xazzer81 <achau1019@gmail.com>
2021-12-05 20:25:29 -0500

    Add commit details, message, author, and commit date

--- web/src/components/Details.jsx
+++ web/src/components/Details.jsx
@@ -4,27 +4,56 @@ import { useEffect, useState } from 'react';
 
 function Details({ name, backClick }) {
   const [content, setContent] = useState('Loading');
+  const [latestCommit, setLatestCommit] = useState(null);
+
   useEffect(() => {
     axios
       .get(`https://raw.githubusercontent.com/${name}/master/README.md`)
       .then((res) => setContent(res.data))
       .catch((err) => setContent('Not Found'));
-  });
+  }, [name]);
+
+  useEffect(() => {
+    axios
+      .get(`https://api.github.com/repos/${name}/commits`)
+      .then((res) => {
+        const sortedCommits = res.data.sort((a, b) => {
+          return (
+            new Date(b.commit.author.date) - new Date(a.commit.author.date)
+          );
+        });
+        setLatestCommit(sortedCommits[0]);
+      })
+      .catch((err) => console.log(err));
+  }, [name]);
+
+  console.log(latestCommit);
 
   return (
     <div className="details">
       <h1>Details</h1>
       <button onClick={backClick}>Back</button>
-      <ul>
-        <li>Name</li>
-        <li>Author</li>
-        <li>Created date</li>
-        <p>
-          <b>Readme: </b>
-          <br />
-          {content}
-        </p>
-      </ul>
+      {latestCommit && (
+        <ul>
+          <li>
+            <b>Message: </b>
+            {latestCommit.commit.message}
+          </li>
+          <li>
+            <b>Author: </b>
+            {latestCommit.commit.author.name}
+          </li>
+          <li>
+            <b>Commit date: </b>
+            {latestCommit.commit.author.date}
+          </li>
+        </ul>
+      )}
+      <p>
+        <b>Readme: </b>
+        <br />
+        {content}
+      </p>
     </div>
   );
 }
